086d65eddd93fb27e1e2649f8377b5d9debd7242
Abhishek Mishra <abhishekmishra0409@gmail.com>
2025-03-24 19:52:24 +0530

    doctors

---
+++ client/src/component/Doctors/DepartmentCardLr.jsx
@@ -0,0 +1,18 @@
+const DepartmentCardLr = ({ icon, title, category, description }) => {
+    return (
+        <div className="w-80 bg-slate-100 shadow-md rounded-xl p-6 text-center border border-gray-200">
+            <div
+                className="w-20 h-20 border-2 border-white rounded-full flex items-center justify-center mx-auto bg-white">
+                <img src={icon} alt={title} className="w-12 h-12"/>
+            </div>
+            <h3 className="text-lg font-semibold text-gray-900">{title}</h3>
+            <p className="text-teal-500 font-medium mt-1">{category}</p>
+            <p className="text-gray-600 text-sm mt-2">{description}</p>
+            <a href="#" className="text-gray-800 font-semibold mt-4 inline-block">
+                Get details..
+            </a>
+        </div>
+    );
+};
+
+export default DepartmentCardLr;
--- client/src/page/Doctors.jsx
+++ client/src/page/Doctors.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import DepartmentCardLr from "../component/Doctors/DepartmentCardLr.jsx";
 
 function Doctors() {
     return (
         <>
-            <div>Doctors</div>
+            <DepartmentCardLr
+                icon="/oncology_icon.svg"
+                title="Oncology"
+                category="Cancer & Tumors"
+                description="Behind the word mountains, far from the countries Vokalia and Consonantia"
+            />
         </>
     );
 }
